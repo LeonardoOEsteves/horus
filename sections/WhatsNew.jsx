77b330cd9bb2512fd96d9ccb89425b8e0bd56182
LeonardoOEsteves <136409539+LeonardoOEsteves@users.noreply.github.com>
2023-06-14 17:02:20 -0300

    Update WhatsNew.jsx

--- sections/WhatsNew.jsx
+++ sections/WhatsNew.jsx
@@ -20,8 +20,8 @@ const WhatsNew = () => (
         variants={fadeIn("right", "tween", 0.2, 1)}
         className='flex-[0.75] flex justify-center flex-col'
       >
-        <TypingText title="| What's New?" />
-        <TitleText title={<>What's new about the Metaversus?</>} />
+        <TypingText title="| O que há de novo?" />
+        <TitleText title={<>O que há de novo no Metaverso Horus?</>} />
         <div className='mt-[48px] flex flex-wrap justify-between gap-[24px]'>
           {newFeatures.map((feature, index) => (
             <NewFeatures key={feature.title} {...feature} />
