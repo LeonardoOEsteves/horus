2b03bb29caef8086ca187a68e33aff47e2fa8f26
LeonardoOEsteves <136409539+LeonardoOEsteves@users.noreply.github.com>
2023-06-14 16:59:21 -0300

    Update About.jsx

--- sections/About.jsx
+++ sections/About.jsx
@@ -16,20 +16,20 @@ const About = () => (
       viewport={{ once: false, amount: 0.25 }}
       className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
     >
-      <TypingText title='| About Metaversus' textStyles='text-center' />
+      <TypingText title='| Sobre o Metaverso' textStyles='text-center' />
 
       <motion.p
         variants={fadeIn("up", "tween", 0.2, 1)}
         className='mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white'
       >
-        <span className='font-extrabold text-white'>Metaverse</span> is a new thing in the
-        future, where you can enjoy the virtual world by feeling like it's really real,
-        you can feel what you feel in this metaverse world, because this is really the{" "}
-        <span className='font-extrabold text-white'>madness of the metaverse</span> of
-        today, using only <span className='font-extrabold text-white'>VR</span> devices
-        you can easily explore the metaverse world you want, turn your dreams into
-        reality. Let's <span className='font-extrabold text-white'>explore</span> the
-        madness of the metaverse by scrolling down
+        <span className='font-extrabold text-white'>No Metaverso HORUS</span> o Roleplay ganha
+        vida de forma excepcional, Ao cruzar a porta para este mundo virtual, você será
+        imerso em uma experiência única, onde sua imaginação se torna realidade. Hoje, você poderá
+        sentir os efeitos da {" "}
+        <span className='font-extrabold text-white'>Loucura do Metaverso,</span> utilizando um dispositivo
+       <span className='font-extrabold text-white'>VR</span> ou apenas clicando no visualizer,
+        você poderá explorar o nosso Metaverso dentro do universo do GTA V FiveM, torne seus sonhos em realidade.
+        Vamos <span className='font-extrabold text-white'>explorar</span> a loucura do metaverso juntos, rolando para baixo
       </motion.p>
 
       <motion.img
