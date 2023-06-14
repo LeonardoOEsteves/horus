b491241d2cb9e87e75a2ca22d0f5b7f59acf4799
LeonardoOEsteves <136409539+LeonardoOEsteves@users.noreply.github.com>
2023-06-14 16:53:07 -0300

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -18,7 +18,7 @@ const Footer = () => (
     <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
       <div className='flex items-center justify-between flex-wrap gap-5'>
         <h4 className='font-bold md:text-[64px] text-[44px] text-white'>
-          Enter the Metaverse
+          Entre no HoRuS Legacy
         </h4>
         <button
           type='button'
@@ -29,7 +29,7 @@ const Footer = () => (
             alt='headset'
             className='w-[24px] h-[24px] object-contain'
           />
-          <span className='font-normal text-[16px] text-white'>ENTER METAVERSE</span>
+          <span className='font-normal text-[16px] text-white'>VISUALIZE NO METAVERSO</span>
         </button>
       </div>
 
@@ -39,7 +39,7 @@ const Footer = () => (
         <div className='flex items-center justify-between flex-wrap gap-4'>
           <h4 className='font-extrabold text-[24px] text-white'>Metaversus</h4>
           <p className='font-normal text-[14px] text-white opacity-50'>
-            Copyright © 2021 - 2022 Metaversus. All rights reserved.
+            Copyright © 2020 - 2023 Metaverso HoRuS. All rights reserved.
           </p>
           <div className='flex gap-4'>
             {socials.map((social) => (
